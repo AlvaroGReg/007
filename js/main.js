be3852f4f31c1ad4c8e40534c070c312f5441d81
AlvaroGReg <alvaro.gracia.reg@gmail.com>
2023-06-27 12:54:00 +0200

    nav optim

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 const nav = document.getElementById('navcontainer')
 
 const openMenu = () => {
-    if (nav.classList.contains('active')){
-        nav.classList.remove('active')
-    }else{
-        nav.classList.add('active')
-    }
+    nav.classList.toggle('active')
 }
